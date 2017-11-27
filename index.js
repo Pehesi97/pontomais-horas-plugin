ed25257647cf762584c45902f2f3a0eee238db6f
Douglas WiseNet <pedro@tracksale.co>
2017-11-27 17:01:03 -0200

    new structure

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var enabledCheckbox = document.getElementById('enabled');
-    checkPageButton.addEventListener('click', function() {
-  
-      chrome.tabs.getSelected(null, function(tab) {
-        d = document;
-  
-        var f = d.createElement('form');
-        f.action = 'http://gtmetrix.com/analyze.html?bm';
-        f.method = 'post';
-        var i = d.createElement('input');
-        i.type = 'hidden';
-        i.name = 'url';
-        i.value = tab.url;
-        f.appendChild(i);
-        d.body.appendChild(f);
-        f.submit();
-      });
-    }, false);
-  }, false);
+var timeLeft = "--:--";
 
-  $(document).ready(function() {
-    $('#enabled').change(function() {        
-        if (this.checked) {
-            $('#horas-faltantes').remove();
-        } else {
-            $('.tabset.profile-tabset').append('<li id="horas-faltantes" class="pull-right"><span>Nips</span></li>');
-        }
-    });
-  });
+var nav = {};
+
+window.onload = function() {
+    setTimeout(function() {
+        nav = $(".navbar-main .navbar-right");        
+        nav.append('<li class="pull-right"><a style="line-height: 40px">' + timeLeft + '</a></li>');        
+        var times = [];
+        var pontos = $(".profile-timeline .media .media-right .label:contains('hoje')").each(function(index) {
+            times.push($(this).text().split(' ')[1]);
+        });
+        times.reverse();
+        console.log(times);        
+    }, 5000);
+};
