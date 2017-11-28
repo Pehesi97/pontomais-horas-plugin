4824299bc82748ced05dc0235e877a696388b16a
Douglas WiseNet <pedro@tracksale.co>
2017-11-28 18:31:59 -0200

    pedrao da extensao

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ var timeLeft = "--:--";
 
 var nav = {};
 
-window.onload = function() {
-    setTimeout(function() {
-        nav = $(".navbar-main .navbar-right");        
-        nav.append('<li class="pull-right"><a style="line-height: 40px">' + timeLeft + '</a></li>');        
-        var times = [];
-        var pontos = $(".profile-timeline .media .media-right .label:contains('hoje')").each(function(index) {
-            times.push($(this).text().split(' ')[1]);
+// trabalho: 0
+// descanso: 1
+
+const TYPE_TRABALHO = 0, TYPE_SAIDA = 1;
+
+setTimeout(function() {
+    nav = $(".navbar-main .navbar-right");        
+    nav.append('<li class="pull-right"><a style="line-height: 40px">' + timeLeft + '</a></li>');    
+
+    var times = [];
+    var pontos = $(".profile-timeline .media .media-right .label:contains('hoje')").each(function(index) {
+        times.push({ 
+            time: $(this).text().split(' ')[1], 
+            type: (index % 2 == 0) ? TYPE_TRABALHO : TYPE_SAIDA
         });
-        times.reverse();
-        console.log(times);        
-    }, 5000);
-};
+    });
+    times.reverse();
+    
+    
+}, 5000);
